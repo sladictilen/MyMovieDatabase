fb10175cb88a903694c2adeb686607ed7581e5fa
Tilen Sladič <sladic.tilen@gmail.com>
2022-04-13 18:57:54 +0200

    added swipe gestures

--- app/src/main/java/com/sladictilen/moviedatabase/ui/presentation/watchedmovies/WatchedMoviesScreen.kt
+++ app/src/main/java/com/sladictilen/moviedatabase/ui/presentation/watchedmovies/WatchedMoviesScreen.kt
@@ -1,94 +1,97 @@
 package com.sladictilen.moviedatabase.ui.presentation.watchedmovies
 
 import android.util.Log
-import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ExperimentalMotionApi
+import androidx.constraintlayout.compose.MotionLayout
+import androidx.constraintlayout.compose.MotionScene
+import androidx.constraintlayout.compose.layoutId
 import com.skydoves.landscapist.glide.GlideImage
+import com.sladictilen.moviedatabase.R
 import kotlin.math.roundToInt
 
 
 @Composable
 fun WatchedMoviesScreen() {
-    // here we use LazyColumn that has build-in nested scroll, but we want to act like a
-// parent for this LazyColumn and participate in its nested scroll.
-// Let's make a collapsing toolbar for LazyColumn
-    val toolbarHeight = 250.dp
-    val toolbarHeightPx = with(LocalDensity.current) { toolbarHeight.roundToPx().toFloat() }
-// our offset to collapse toolbar
-    val toolbarOffsetHeightPx = remember { mutableStateOf(0f) }
-// now, let's create connection to the nested scroll system and listen to the scroll
-// happening inside child LazyColumn
-    val nestedScrollConnection = remember {
-        object : NestedScrollConnection {
-            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
-                // try to consume before LazyColumn to collapse toolbar if needed, hence pre-scroll
-                val delta = available.y
-                val newOffset = toolbarOffsetHeightPx.value + delta
-
-                toolbarOffsetHeightPx.value = newOffset.coerceIn(-toolbarHeightPx + (128), 0f)
-                // here's the catch: let's pretend we consumed 0 in any case, since we want
-                // LazyColumn to scroll anyway for good UX
-                // We're basically watching scroll without taking it
-                return Offset.Zero
-            }
+    Column {
+        var progress by remember {
+            mutableStateOf(0f)
         }
+        ProfileHeader(progress = progress)
+        Spacer(modifier = Modifier.height(32.dp))
+        Slider(
+            value = progress,
+            onValueChange = {
+                progress = it
+            },
+            modifier = Modifier.padding(horizontal = 32.dp)
+        )
     }
-    Box(
-        Modifier
-            .fillMaxSize()
-            // attach as a parent to the nested scroll system
-            .nestedScroll(nestedScrollConnection)
+
+}
+
+@OptIn(ExperimentalMotionApi::class)
+@Composable
+fun ProfileHeader(progress: Float) {
+    val context = LocalContext.current
+    val motionScene = remember {
+        context.resources
+            .openRawResource(R.raw.motion_scene)
+            .readBytes()
+            .decodeToString()
+    }
+    MotionLayout(
+        motionScene = MotionScene(content = motionScene),
+        progress = progress,
+        modifier = Modifier.fillMaxWidth()
     ) {
-        // our list with build in nested scroll support that will notify us about its scroll
-        LazyColumn(contentPadding = PaddingValues(top = toolbarHeight)) {
-            items(100) { index ->
-                Text(
-                    "I'm item $index", modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                )
-            }
-        }
-        TopAppBar(
+        val properties = motionProperties(id = "profile_pic")
+        Box(
             modifier = Modifier
-                .height(toolbarHeight)
-                .offset { IntOffset(x = 0, y = toolbarOffsetHeightPx.value.roundToInt()) },
-            title = {
-                GlideImage(
-                    imageModel = "https://image.tmdb.org/t/p/w780/en971MEXui9diirXlogOrPKmsEn.jpg",
-                    contentScale = ContentScale.FillWidth,
-                    modifier = Modifier,
-                    loading = {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxSize(),
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.Center
-                        ) {
-                            CircularProgressIndicator(color = MaterialTheme.colors.primary)
-                        }
-                    }
+                .fillMaxWidth()
+                .background(Color.DarkGray)
+                .layoutId("box")
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_imdb),
+            contentDescription = null,
+            modifier = Modifier
+                .clip(CircleShape)
+                .border(
+                    width = 2.dp,
+                    color = properties.value.color("background"),
+                    shape = CircleShape
                 )
-            }
+                .layoutId("profile_pic")
+        )
+        Text(
+            text = "Philipp Lackner",
+            fontSize = 24.sp,
+            modifier = Modifier.layoutId("username"),
+            color = properties.value.color("background")
         )
     }
 }
