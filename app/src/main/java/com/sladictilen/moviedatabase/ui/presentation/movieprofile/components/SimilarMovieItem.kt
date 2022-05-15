b053bc43236c7ebcbf8f32399e3fd57e2ffbc873
Tilen Sladič <sladic.tilen@gmail.com>
2022-05-15 18:32:16 +0200

    bug fix

--- app/src/main/java/com/sladictilen/moviedatabase/ui/presentation/movieprofile/components/SimilarMovieItem.kt
+++ app/src/main/java/com/sladictilen/moviedatabase/ui/presentation/movieprofile/components/SimilarMovieItem.kt
@@ -2,9 +2,11 @@ package com.sladictilen.moviedatabase.ui.presentation.movieprofile.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.skydoves.landscapist.glide.GlideImage
@@ -13,8 +15,9 @@ import com.skydoves.landscapist.glide.GlideImage
 fun SimilarMovieItem(posterUrl: String, title: String, onClick: () -> Unit) {
     Box(
         modifier = Modifier
-            .width(100.dp)
+            .width(150.dp)
             .clickable { onClick() }
+            .clip(RoundedCornerShape(10.dp))
     ) {
         Column(
             modifier = Modifier
@@ -23,7 +26,7 @@ fun SimilarMovieItem(posterUrl: String, title: String, onClick: () -> Unit) {
             Row() {
                 GlideImage(
                     imageModel = "https://image.tmdb.org/t/p/w185$posterUrl",
-                    contentScale = ContentScale.FillWidth
+                    contentScale = ContentScale.FillHeight
                 )
             }
             Row() {
