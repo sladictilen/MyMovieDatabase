a0ef7504001869e8adb86e14f7713e60adf633c0
Tilen Sladič <sladic.tilen@gmail.com>
2022-05-17 15:43:58 +0200

    bug fix

--- app/src/main/java/com/sladictilen/moviedatabase/ui/presentation/watchedmovies/WatchedMoviesScreen.kt
+++ app/src/main/java/com/sladictilen/moviedatabase/ui/presentation/watchedmovies/WatchedMoviesScreen.kt
@@ -1,37 +1,7 @@
 package com.sladictilen.moviedatabase.ui.presentation.watchedmovies
 
-import android.util.Log
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
-import androidx.compose.ui.input.nestedscroll.NestedScrollSource
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ExperimentalMotionApi
-import androidx.constraintlayout.compose.MotionLayout
-import androidx.constraintlayout.compose.MotionScene
-import androidx.constraintlayout.compose.layoutId
-import com.skydoves.landscapist.glide.GlideImage
-import com.sladictilen.moviedatabase.R
-import kotlin.math.roundToInt
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 
 
 @Composable
