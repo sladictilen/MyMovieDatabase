9c5c5a9d6d6fa50583813c7ed688dd7dc1c8886e
Tilen Sladič <sladic.tilen@gmail.com>
2022-05-18 19:00:04 +0200

    bug fixes

--- app/src/main/java/com/sladictilen/moviedatabase/ui/presentation/watchedmovies/WatchedMoviesScreen.kt
+++ app/src/main/java/com/sladictilen/moviedatabase/ui/presentation/watchedmovies/WatchedMoviesScreen.kt
@@ -2,10 +2,17 @@ package com.sladictilen.moviedatabase.ui.presentation.watchedmovies
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import com.sladictilen.moviedatabase.ui.presentation.watchedmovies.components.WatchedMovieItem
 
 
 @Composable
 fun WatchedMoviesScreen() {
-    Text(text = "Watched Movies Screen")
 
+    WatchedMovieItem(
+        "/k9tJGdMkzOe17YH2ZCQjNnX5YLz.jpg",
+        "Deadpool",
+        "Ryan Reynolds, in ostali",
+        "22.3.2022",
+        "2"
+    )
 }
--- app/src/main/java/com/sladictilen/moviedatabase/ui/presentation/watchedmovies/components/WatchedMovieItem.kt
+++ app/src/main/java/com/sladictilen/moviedatabase/ui/presentation/watchedmovies/components/WatchedMovieItem.kt
@@ -1,11 +1,14 @@
 package com.sladictilen.moviedatabase.ui.presentation.watchedmovies.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,35 +24,39 @@ fun WatchedMovieItem(
     watchedDate: String,
     userRating: String
 ) {
-    // TODO Movie rating scale - Very bad, Bad, Okay, Great, Excelent
-    Row(
-        modifier = Modifier
-            .height(100.dp)
-            .fillMaxWidth()
-            .clip(RoundedCornerShape(5.dp))
-    ) {
-        // Img
-        Column(modifier = Modifier.fillMaxHeight()) {
-            GlideImage(
-                imageModel = "https://image.tmdb.org/t/p/w185${posterUrl}",
-                contentScale = ContentScale.FillHeight,
-            )
-        }
-        // movie info
-        Column() {
-            Row() {
-                Text(text = title)
+    // TODO Movie rating scale - Very bad, Bad, Okay, Great, Excellent
+    Box(modifier = Modifier.background(Color.DarkGray)) {
+
+
+        Row(
+            modifier = Modifier
+                .height(100.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(5.dp))
+        ) {
+            // Img
+            Column(modifier = Modifier.fillMaxHeight().border(1.dp, Color.Green).width(100.dp)) {
+                GlideImage(
+                    imageModel = "https://image.tmdb.org/t/p/w185${posterUrl}",
+                    contentScale = ContentScale.FillHeight,
+                )
             }
-            Row() {
-                Text(text = actors)
+            // movie info
+            Column(modifier = Modifier.fillMaxHeight().border(1.dp, Color.Red)) {
+                Row() {
+                    Text(text = title)
+                }
+                Row() {
+                    Text(text = actors)
+                }
+                Row() {
+                    Text(text = watchedDate)
+                }
             }
-            Row() {
-                Text(text = watchedDate)
+            // user rating
+            Column(modifier = Modifier.fillMaxHeight().border(1.dp, Color.Blue)) {
+                GlideImage(imageModel = getUserRatingImage(userRating), Modifier.size(40.dp))
             }
-        }
-        // user rating
-        Column(modifier = Modifier.fillMaxHeight()) {
-            GlideImage(imageModel = getUserRatingImage(userRating))
         }
     }
 
