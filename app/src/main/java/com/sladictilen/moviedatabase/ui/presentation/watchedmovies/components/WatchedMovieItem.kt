edc53852724a6b7a392bd6535caf0f717d904fb5
Tilen Sladič <sladic.tilen@gmail.com>
2022-05-19 21:28:47 +0200

    bug fix

--- app/src/main/java/com/sladictilen/moviedatabase/ui/presentation/watchedmovies/components/WatchedMovieItem.kt
+++ app/src/main/java/com/sladictilen/moviedatabase/ui/presentation/watchedmovies/components/WatchedMovieItem.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -34,15 +35,15 @@ fun WatchedMovieItem(
                 .fillMaxWidth()
                 .clip(RoundedCornerShape(5.dp))
         ) {
-            // Img
-            Column(modifier = Modifier.fillMaxHeight().border(1.dp, Color.Green).width(100.dp)) {
+            // Poster
+            Column(modifier = Modifier.fillMaxHeight().border(1.dp, Color.Green).width(70.dp)) {
                 GlideImage(
                     imageModel = "https://image.tmdb.org/t/p/w185${posterUrl}",
                     contentScale = ContentScale.FillHeight,
                 )
             }
             // movie info
-            Column(modifier = Modifier.fillMaxHeight().border(1.dp, Color.Red)) {
+            Column(modifier = Modifier.fillMaxHeight().padding(5.dp).border(1.dp, Color.Red)) {
                 Row() {
                     Text(text = title)
                 }
@@ -54,8 +55,8 @@ fun WatchedMovieItem(
                 }
             }
             // user rating
-            Column(modifier = Modifier.fillMaxHeight().border(1.dp, Color.Blue)) {
-                GlideImage(imageModel = getUserRatingImage(userRating), Modifier.size(40.dp))
+            Column(modifier = Modifier.fillMaxHeight().border(1.dp, Color.Blue), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.End) {
+                GlideImage(imageModel = getUserRatingImage(userRating), Modifier.size(60.dp))
             }
         }
     }
@@ -68,7 +69,7 @@ fun Prev22() {
     WatchedMovieItem(
         "/k9tJGdMkzOe17YH2ZCQjNnX5YLz.jpg",
         "Deadpool",
-        "Ryan Reynolds, in ostali",
+        "Ryan Reynolds, in ostali šalabajzerji",
         "22.3.2022",
         "2"
     )
