ec29d940d563c03111f95d87f654db6ba6512e23
Tilen Sladič <sladic.tilen@gmail.com>
2022-05-20 22:18:26 +0200

    bug fix

--- app/src/main/java/com/sladictilen/moviedatabase/util/Constants.kt
+++ app/src/main/java/com/sladictilen/moviedatabase/util/Constants.kt
@@ -2,7 +2,7 @@ package com.sladictilen.moviedatabase.util
 
 class Constants {
     companion object {
-        const val TMDB_API_KEY = "a45d21208d507b4fa85de6f10cf9b30e"
+        const val TMDB_API_KEY = "a45d21208d507b4fa85de6f10cf9b30etet"
         const val TMDB_BASE_URL = "https://api.themoviedb.org/3/"
 
         const val OMDB_API_KEY = "938244be"
